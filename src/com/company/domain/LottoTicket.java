7d4c18f9d6260242e41eede1b688ba2f81d10788
Kyle Choi <tmdfl1027@gmail.com>
2022-02-21 12:13:32 +0900

    [step1] 2번째 커밋 오류 수정
    2번째 커밋에서 add를 안 해주어 변경 내용 누락. 동일한 내용으로 재커밋

--- src/com/company/domain/LottoTicket.java
+++ src/com/company/domain/LottoTicket.java
@@ -1,4 +1,13 @@
 package com.company.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoTicket {
+    private final List<Integer> lottoTicket;
+
+    public LottoTicket(List<Integer> lottoTicket) {
+        this.lottoTicket = lottoTicket;
+    }
+
 }
--- src/com/company/domain/TicketOffice.java
+++ src/com/company/domain/TicketOffice.java
@@ -1,2 +1,21 @@
-package com.company.domain;public class TicketOffice {
+package com.company.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketOffice {
+    private final List<Integer> lottoNumber = new ArrayList<Integer>();
+    private final int MAX_NUMBER = 45;
+
+    public TicketOffice() {
+        setLottoNumber();
+    }
+
+    private void setLottoNumber() {
+        for (int i = 1; i <= MAX_NUMBER; i++) {
+           this.lottoNumber.add(i);
+        }
+    }
+
+
 }
